5f8c63c080254dcc2d1873aab18e36eb673884f9
Thet Myo Khaing <thetmyokhaing@Thets-MacBook-Pro.local>
2023-09-01 11:30:07 +0630

    telegram sidebar ui in kotlin xml

---
+++ app/src/main/java/com/example/telegram_side_ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.telegram_side_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById<Toolbar>(R.id.tbMain))
+        supportActionBar?.title = ""
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+}
